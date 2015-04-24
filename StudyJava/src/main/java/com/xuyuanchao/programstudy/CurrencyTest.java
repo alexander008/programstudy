c289cba141fcecf44ad1b6cb2b0a2c9871e87892
Xu Yuanchao <Alexander.xuchao@gmail.com>
2015-04-24 15:20:39 +0800

    mysql db design examle

---
+++ StudyJava/src/main/java/com/xuyuanchao/programstudy/CurrencyTest.java
@@ -0,0 +1,12 @@
+package com.xuyuanchao.programstudy;
+
+import java.util.Currency;
+import java.util.Locale;
+
+public class CurrencyTest {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Currency.getInstance("JPY"));
+	}
+}
